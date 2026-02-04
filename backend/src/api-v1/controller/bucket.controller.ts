95113bf1747f2fded6fed4c34515f3f4735e90fe
shreedhar01 <shreedharkalamagar@gmail.com>
2026-02-04 09:47:11 +0545

    fix: right message if not updated

--- backend/src/api-v1/controller/bucket.controller.ts
+++ backend/src/api-v1/controller/bucket.controller.ts
@@ -5,8 +5,11 @@ import { postLengthService } from "../../services/bucket.service.js"
 export const patchLength = async (req: Request<{}, {}, PostLength>, res: Response) => {
     const { name, length } = req.body
     try {
-        await postLengthService(name, length);
-        res.json({ message: "Volume edited successfully" });
+        const isUpdated = await postLengthService(name, length);
+        if (!isUpdated) {
+            res.status(500).json({ message: "Failed to edit volume" });
+        }
+        res.status(202).json({ message: "Volume edited successfully" });
     } catch (error) {
         console.error("Error updating volume:", error);
         res.status(500).json({ message: "Failed to edit volume" });
--- backend/src/services/bucket.service.ts
+++ backend/src/services/bucket.service.ts
@@ -5,7 +5,9 @@ import { Buckets } from "../types/bucket-name.js";
 
 export const postLengthService = async (name: Buckets, length: number) => {
     // const value = Buckets[name];
-    console.log(`name :: ${name} number :: ${length}`)
-    await db.update(bucketTable).set({ length: length }).where(eq(bucketTable.name, name))
+    // console.log(`name :: ${name} number :: ${length}`)
+    const isUpdated = await db.update(bucketTable).set({ length: length }).where(eq(bucketTable.name, name))
+    console.log("isUpdated :: ", isUpdated)
+    if(isUpdated?.rowCount === 0) return false
     return true
 }
